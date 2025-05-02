67daf7efabd49ff2afb8e9103a6b4265774cbbe6
Mércia Martins <merciamartins07@gmail.com>
2025-05-02 14:31:39 -0300

    Initial commit

---
+++ Elevador/Elevador.ino
@@ -0,0 +1,23 @@
+#include <Drive.h>
+#include <Stepper.h>
+#include <AccelStepper.h>
+
+int STCP_pin = 32;
+int SHCP_pin = 30;
+int DS_pin = 34;
+
+//PONTE H - MOTOR
+#define N1 37
+#define N2 35
+
+void setup() {
+  //MOTOR INTERNO E SENSORES
+  
+  pinMode(STCP_pin, OUTPUT);
+  pinMode(SHCP_pin, OUTPUT);
+  pinMode(DS_pin, OUTPUT);
+}
+
+void loop() {
+  ligarMotor(1);
+}
---
+++ Elevador/motor-func.ino
@@ -0,0 +1,17 @@
+//parar o motor:
+
+void pararMotor(){
+  digitalWrite(N1, HIGH);
+  digitalWrite(N2, HIGH);
+}
+
+void ligarMotor(int num){ //0 subir ,  1 descer
+
+  if(num){
+      digitalWrite(N1, HIGH);//sentido horario
+      digitalWrite(N2, LOW);
+  }else{
+      digitalWrite(N1, LOW);
+      digitalWrite(N2, HIGH);//girar no sentido anti-horario
+  }
+}
